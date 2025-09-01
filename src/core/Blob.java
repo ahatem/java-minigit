91437382b3a9eba9c6589cb81f6ba58833cf991f
Ahmed Hatem <ahatem925@gmail.com>
2025-09-01 23:16:57 +0300

    feat(blob): add toRaw method for Blob object
    - implement toRaw method to generate the raw byte representation of a blob object
    - include object type and size in the header followed by null byte and content

--- src/core/Blob.java
+++ src/core/Blob.java
@@ -15,4 +15,15 @@ public record Blob(ObjectId id, byte[] content) implements GitObject {
     public String toString() {
         return new String(content);
     }
+
+    public byte[] toRaw() {
+        String header = "blob " + content.length + "\0";
+        byte[] headerBytes = header.getBytes();
+
+        byte[] raw = new byte[headerBytes.length + content.length];
+        System.arraycopy(headerBytes, 0, raw, 0, headerBytes.length);
+        System.arraycopy(content, 0, raw, headerBytes.length, content.length);
+
+        return raw;
+    }
 }
